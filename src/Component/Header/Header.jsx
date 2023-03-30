cf44df3063a36e54a6a5ece4fe36abf31001818c
AhaD <abuahad35188@gmail.com>
2023-03-30 23:03:22 +0600

    first commit

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+     return (
+          <div className='header'>
+               <nav className='navber'>
+                    <a href="/">ForntEnd</a>
+                    <a href="/">ForntEnd</a>
+                     
+               </nav>
+          </div>
+     );
+};
+
+export default Header;
